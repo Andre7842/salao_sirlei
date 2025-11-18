78efad916160397f9478dec170d625ce3652d0d3
Andre7842 <andrekhan.vs334@gmail.com>
2025-11-17 23:56:59 -0300

    Atualizar o firebase.js

--- firebase.js
+++ firebase.js
@@ -1,28 +1,10 @@
-<!-- firebase.js -->
-<!-- Inclua este script antes de script.js e script-admin.js em cada página -->
-<script src="https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.22.2/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore-compat.js"></script>
-
-<script>
-  // Config do Firebase que você enviou
-  const firebaseConfig = {
-    apiKey: "AIzaSyB8zRbSH1nB8NmqeEFj7iDBZB9RqfufflE",
-    authDomain: "sirlei-studios.firebaseapp.com",
-    projectId: "sirlei-studios",
-    storageBucket: "sirlei-studios.firebasestorage.app",
-    messagingSenderId: "107743588118",
-    appId: "1:107743588118:web:bc5da05f327ef094412a37",
-    measurementId: "G-53YWDTQ1JL"
-  };
-
-  // Inicializa
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-  const db = firebase.firestore();
-
-  // Disponibiliza globalmente
-  window.firebaseApp = firebase;
-  window.auth = auth;
-  window.db = db;
-</script>
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
